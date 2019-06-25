f97fb70e2a6c5be61e207467f7ba93bb032d0acf
SAURABH <kvsaurav@users.noreply.github.com>
2019-06-25 21:26:17 +0530

    Add files via upload

---
+++ sub_find.py
@@ -0,0 +1,26 @@
+#test1 for dnsdumpster 
+#python2
+
+import dnsdumpster
+import os
+
+from dnsdumpster.DNSDumpsterAPI import DNSDumpsterAPI #USING DNSDUMPSTER.COM API
+print "ENTER DOMAIN TO BE SEARCHED"
+g= raw_input('')
+res = DNSDumpsterAPI({'verbose': True}).search(g)
+
+#print res.keys()
+
+#print res['dns_records']['host']
+# print res
+
+print res.keys() 
+
+for item in res['dns_records']['host']:
+
+	print "*"*80
+	print "\n\n"
+	print "DOMAIN: ", item['domain'].replace('<br', '')
+	print "IP ADRESS: ",item['ip']
+	print "PROVIDER :",item['provider']
+	print "REVERSE DNS :",item['reverse_dns']
